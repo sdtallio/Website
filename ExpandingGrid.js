df224d596c53c975d21a469463d21806ec59b23b
sdtallio <69528428+sdtallio@users.noreply.github.com>
2020-08-19 15:54:17 -0700

    New

--- ExpandingGrid.js
+++ ExpandingGrid.js
@@ -1,4 +1,4 @@
-
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 function openTab(tabName) {
   var i, x;
   x = document.getElementsByClassName("containerTab");
